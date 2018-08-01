28d3b66614cdf7a3263a586563757fb949414852
Haley Someone <hbarlar1@gmail.com>
2018-08-01 23:57:47 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,12 +2,9 @@ require 'pry'
 class Hash
   def keys_of(arguments)
     array = []
-    
     animals.each do |key, value|
-      binding.pry
       if value == arguments
         array << key
-        
   end
   end
   return array
