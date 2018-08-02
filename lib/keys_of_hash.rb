fae3ce8359cebf45ff49cca1e496605a64108dc1
Haley Someone <hbarlar1@gmail.com>
2018-08-02 00:20:47 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,9 +2,9 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     array = []
-    arguments.each do |x|
+    arguments.each do |one_argument|
       self.each do |key, value|
-        if x == value
+        if one_argument == value
           array << key
         end
       end
